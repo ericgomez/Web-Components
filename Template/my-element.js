ebe5ae36bfd7cf3816d04de8f180bd129ddb92d7
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-16 15:52:13 -0600

    Template

---
+++ Template/my-element.js
@@ -0,0 +1,36 @@
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+  // Metodo getTemplate
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+      <section>
+        <h2 class="title">Hola mundo!</h2>
+        <div>
+          <p>Soy más texto ejemplo</p>
+        </div>
+      </section>
+      ${this.getStyles()}
+    `;
+    return template;
+  }
+  // Metodo getStyles
+  getStyles() {
+    return `
+      <style>
+        h2 {
+          color: red;
+        }
+      </style>
+    `;
+  }
+  render() {
+    this.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+  connectedCallback() {
+    this.render();
+  }
+}
+customElements.define("my-element", myElement);
